600dfa56ee736ff941120b5511acf6142c51ab90
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-09-22 20:43:49 -0500

    Update findKthLargest.py

--- findKthLargest.py
+++ findKthLargest.py
@@ -1,2 +1,9 @@
 #215. Kth Largest Element in an Array
 
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
